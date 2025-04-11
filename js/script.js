4c4d0d0645f83e1d6634c31f86b2334db57f614e
EneaSema <sema.enea1993@gmail.com>
2025-04-11 16:38:46 +0200

    work to developer-exercise

--- js/script.js
+++ js/script.js
@@ -4,15 +4,17 @@ console.log("ciao");
 
 const elementCountadown = document.getElementById("countdown");
 console.log(elementCountadown);
-
-for (let i = 0; i < timeLimit; i - 1000) {
-  let timeLimit = 1000 * 30;
+// creo variabile per timer
+let timeLimit = 1000 * 30;
+// creato ciclo for per timer con condizione di scadenza e verifica del tempo trascorso
+for (let i = timeLimit; i < timeLimit; i - 1000) {
+  // verifica del tempo trascorso, se uguale a 0
   if (timeLimit[i] === 0) {
+    // si attiva il setTimeout con il cambio da display none a block e comparsa sezione form
     setTimeout(function () {
       const ElementAnswerForm = document.getElementById("answers-form");
       ElementAnswerForm.remove("d-none");
       clearTimeout(timeLimit);
     }, timeLimit);
   }
-  return;
 }
